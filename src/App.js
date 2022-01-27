7b85bdab9a0f5c475d95b665f64b8670f489a27a
ktl28 <44536392+ktl28@users.noreply.github.com>
2022-01-27 11:55:28 -0800

    put a message here

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="header">
+        <div className="logo" />
+        <span className="title">CHATTER!</span>
+      </header>
+      
+      <div id="container">
+        <div className="box text point">hello this is a message</div>
+        <div className="box text point">hello</div>
+        <div className="box text point">yuppie</div>
+      </div>
+
+      <footer className="footer">
+        <input className="text-input" />
+        <button className="send">↑</button>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
